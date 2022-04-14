9e72983fb08db544c9ab79c21332de4cf85d1bdf
SubeetKD <SubeetKD@users.noreply.github.com>
2022-04-14 11:39:54 +0530

    updated config

--- init.el
+++ init.el
@@ -8,7 +8,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(centaur-tabs counsel-projectile projectile counsel ivy-rich ivy neotree doom-modeline doom-themes all-the-icons which-key general evil-org evil-collection evil org-bullets yasnippet lsp-java lsp-mode company use-package)))
+   '(lsp-ui transpose-frame flycheck tree-sitter-langs vterm centaur-tabs counsel-projectile projectile counsel ivy-rich ivy neotree doom-modeline doom-themes all-the-icons which-key general evil-org evil-collection evil org-bullets yasnippet lsp-java lsp-mode company use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
